85b464fa9c63102855664f22622187a4049ecbe1
Hendrik Weiler <hendrik-weiler@users.noreply.github.com>
2023-09-16 17:50:03 +0200

    Update append_string.c

--- append_string.c
+++ append_string.c
@@ -1,6 +1,16 @@
 #include <stdlib.h>
 #include <string.h>
 
+/**
+ * Appends a string to another string
+ * 
+ * Example:
+ * char* base_str = NULL;
+ * append(&base_str, "my string to append");
+ * 
+ * @param str The char pointer array
+ * @param append_str The string to append
+*/
 void append(char**str, char*append_str) {
     if(*str == NULL) *str = malloc(1);
     *str = realloc(*str, strlen(*str)+strlen(append_str)+1);
